1324e55f749aa7429c373aef1aa9dc20b57f50b4
kristian.fantin <kristianfantin@gmail.com>
2018-10-21 00:52:57 -0300

    [Backender] - First Commit
    - Configuration of Sources Placeholders - properties messages enable
    - application.yml created instead application.properties - because the read of yml files is easier than properties files.
      --> That choice was done just to show an alternative for properties files - but the result is the same.
    
    Dev: Kristian Fantin Pereira.

---
+++ src/main/java/com/glovoapp/backender/config/SourcesPlaceholderConfiguration.java
@@ -0,0 +1,17 @@
+package com.glovoapp.backender.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+@Configuration
+public class SourcesPlaceholderConfiguration {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer sourcesPlaceholderProperties() {
+        PropertySourcesPlaceholderConfigurer placeholderConfigurer = new PropertySourcesPlaceholderConfigurer();
+        placeholderConfigurer.setIgnoreUnresolvablePlaceholders(true);
+        return placeholderConfigurer;
+    }
+
+}
